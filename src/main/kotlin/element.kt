bec22716943b35c88d649a701300a64648db4b73
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-11 14:39:00 +0300

    Fix a tiny bug

--- src/main/kotlin/element.kt
+++ src/main/kotlin/element.kt
@@ -14,17 +14,18 @@ class CheckedItem(val isChecked: Boolean) : MDElement {
 
 class OrderedList : MDElement {
     val items = ArrayList<ListItem>()
-    override fun toString(): String = items.mapIndexed { i, v -> "${i + 1}. $v" }.joinToString("\n")
+    override fun toString(): String = items.mapIndexed { i, v -> "${i + 1}. $v" }.joinToString("\n") + "\n"
 }
 
 class UnorderedList : MDElement {
     val items = ArrayList<ListItem>()
-    override fun toString(): String = items.map { v -> "- $v" }.joinToString("\n")
+    override fun toString(): String = items.map { v -> "- $v" }.joinToString("\n") + "\n"
 }
 
 class CheckList : MDElement {
     val items = ArrayList<CheckedItem>()
-    override fun toString(): String = items.map { v -> "- [${if (v.isChecked) "x" else " "}] $v" }.joinToString("\n")
+    override fun toString(): String =
+        items.map { v -> "- [${if (v.isChecked) "x" else " "}] $v" }.joinToString("\n") + "\n"
 }
 
 class Table : MDElement {
@@ -32,7 +33,7 @@ class Table : MDElement {
     val rows = ArrayList<TableRow>()
     override fun toString(): String = "\n" + headers.toString() + "\n" +
             "| ----------- ".repeat(headers.cols.size) + "|" + "\n" +
-            rows.map { it.toString() }.joinToString("\n")
+            rows.map { it.toString() }.joinToString("\n") + "\n"
 }
 
 class TableRow : MDElement {
